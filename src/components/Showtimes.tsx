8c46dc05a78629fac71593187229a3bba7f33777
gnbstud <gnb14290@uga.edu>
2025-09-23 15:26:19 -0400

    added showtime component

---
+++ src/components/Showtimes.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+
+interface showtimesProps {
+    id: number;
+    title: string;
+    times: string[] //times can be changed to any better data type
+    //possibly add functions that the showtimes card uses
+}
+
+const MovieDetails: React.FC<showtimesProps> = ({
+    id,
+    title,
+    times
+}) => {
+    //Where the html comes into play
+    return (
+        <div>
+            <p>screen-filling card that displays the available showtimes of
+                the selected movie. Clicking a showtime brings the user to
+                a different page where the actual ordering of tickets comes into play.
+            </p>
+        </div>
+    )
+}
